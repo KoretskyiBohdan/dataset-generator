05b96384639daa75891a2c537e8bc7fe0f59c302
Koretskyi Bohdan <koretzkiy.bogdan@gmail.com>
2023-07-23 21:10:53 -0400

    Added "recursiveResolover" as an open for extension solution inside "createObjectFromShape"

--- src/createObjectFromShape.ts
+++ src/createObjectFromShape.ts
@@ -8,44 +8,48 @@ import { isFunction, isString, isObject, isDefinedType } from './guards';
 import { ShapeType, ResultType, AnyObject } from './types';
 
 interface ICreateObjectFromShape {
-  <T extends ShapeType>(shape: T, index?: number, ref?: AnyObject): ResultType<T>;
+  <T extends ShapeType>(shape: T, index?: number): ResultType<T>;
 }
 /**
  *
  * @param shape - data source and shape reference
  * @param index - current object index
- * @param ref - reference to the the root object (using when object has a nestled objects)
  * @returns {object}
  */
-const createObjectFromShape: ICreateObjectFromShape = (shape, index = 0, ref) => {
-  const obj: AnyObject = {};
-  // if we are on the top level than root is this level object
-  const root = ref || obj;
+const createObjectFromShape: ICreateObjectFromShape = (shape, index = 0) => {
+  // "shape" object can have nested levels, so need to use recursive resolver
+  const recursiveResolver = (currentLevelShape: ShapeType, ref?: AnyObject) => {
+    const currentLevelObj = {};
+    // if we are on the top level than root is this level object
+    const root = ref || currentLevelObj;
 
-  for (const key in shape) {
-    if (!Object.hasOwn(shape, key)) continue;
+    for (const key in currentLevelShape) {
+      if (!Object.hasOwn(currentLevelShape, key)) continue;
 
-    const value = shape[key];
-    let resolvedValue = null;
+      const value = currentLevelShape[key];
+      let resolvedValue = null;
 
-    if (Array.isArray(value)) {
-      resolvedValue = getRandomArrayValue(value);
+      if (Array.isArray(value)) {
+        resolvedValue = getRandomArrayValue(value);
 
-      if (isString(resolvedValue) && isItHasAReference(resolvedValue)) {
-        resolvedValue = resolveReference(root, resolvedValue);
+        if (isString(resolvedValue) && isItHasAReference(resolvedValue)) {
+          resolvedValue = resolveReference(root, resolvedValue);
+        }
+      } else if (isFunction(value)) {
+        resolvedValue = value(index);
+      } else if (isObject(value)) {
+        resolvedValue = recursiveResolver(value, root);
+      } else if (isDefinedType(value)) {
+        resolvedValue = definedTypeResolver(value, index);
       }
-    } else if (isFunction(value)) {
-      resolvedValue = value(index);
-    } else if (isObject(value)) {
-      resolvedValue = createObjectFromShape(value, index, root);
-    } else if (isDefinedType(value)) {
-      resolvedValue = definedTypeResolver(value, index);
+
+      currentLevelObj[key] = resolvedValue;
     }
 
-    obj[key] = resolvedValue;
-  }
+    return currentLevelObj as ResultType<typeof shape>;
+  };
 
-  return obj as ResultType<typeof shape>;
+  return recursiveResolver(shape);
 };
 
 export default createObjectFromShape;
