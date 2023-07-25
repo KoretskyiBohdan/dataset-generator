d8a50c65a90cd497a797575b203cc49229c84769
Koretskyi Bohdan <koretzkiy.bogdan@gmail.com>
2023-07-24 23:55:42 -0400

    Fixed defined type guard return type

--- src/guards.ts
+++ src/guards.ts
@@ -1,7 +1,7 @@
 import { AnyFn, AnyObject } from './types';
-import { DEFINED } from './defined';
+import { DEFINED, DefinedTypeValue } from './defined';
 
-const LIST_OF_DEFINED = Object.keys(DEFINED).map<symbol>((key) => DEFINED[key]);
+const LIST_OF_DEFINED = Object.keys(DEFINED).map<DefinedTypeValue>((key) => DEFINED[key]);
 
 export const isObject = (v: any): v is AnyObject => {
   return Object.prototype.toString.call(v) === '[object Object]';
@@ -11,4 +11,4 @@ export const isFunction = (v: any): v is AnyFn => typeof v === 'function';
 
 export const isString = (v: any): v is string => typeof v === 'string';
 
-export const isDefinedType = (v: any): v is symbol => LIST_OF_DEFINED.includes(v);
+export const isDefinedType = (v: any): v is DefinedTypeValue => LIST_OF_DEFINED.includes(v);
